76e3fc96ede09e7f19b8f05e62a26cf3dcc6adc9
Daniel Gabbay <daniel.g@sps.co.il>
2023-01-13 13:09:13 +0200

    Test Vercel Deployment v17 - change build script
        "dev": "./node_modules/nodemon/bin/nodemon.js",

--- server-v2/src/index.ts
+++ server-v2/src/index.ts
@@ -1,36 +1,19 @@
-// import * as http from "http";
-// import App from "./App";
-//
-// require('dotenv').config()
-//
-// const port = process.env.PORT || 8080;
-//
-// App.set("port", port);
-// const server = http.createServer(App);
-// server.listen(port);
-// module.exports = App;
-//
-// server.on("listening", function (): void {
-//     const addr = server.address();
-//     const bind = (typeof addr === "string") ? `pipe ${addr}` : `port ${addr.port}`;
-//     console.log(`Listening on ${bind}`);
-// });
-//
-//
+import * as http from "http";
+import App from "./App";
 
-import express, {Request, Response} from 'express'
+require('dotenv').config()
 
-const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
-app.get('/', (_req: Request, res: Response) => {
-    return res.send('Express Typescript on Vercel')
-})
+App.set("port", port);
+const server = http.createServer(App);
+server.listen(port);
+module.exports = App;
 
-app.get('/ping', (_req: Request, res: Response) => {
-    return res.send('pong 🏓')
-})
+server.on("listening", function (): void {
+    const addr = server.address();
+    const bind = (typeof addr === "string") ? `pipe ${addr}` : `port ${addr.port}`;
+    console.log(`Listening on ${bind}`);
+});
 
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`)
-})
+
